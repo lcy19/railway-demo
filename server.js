d7145294400e411ae569f394fb09e55dae9a1336
lcy <1901210420@pku.edu.cn>
2023-03-08 18:05:23 +0800

    epoch 1

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./views/index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
